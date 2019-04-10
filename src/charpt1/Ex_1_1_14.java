365e77b1cb35e5b38cddc9f7b6ab126bac7d4a58
liuya <liuya@baidu.com>
2019-04-10 10:42:25 +0800

    add 1.1.14

---
+++ src/charpt1/Ex_1_1_14.java
@@ -0,0 +1,26 @@
+package charpt1;
+
+public class Ex_1_1_14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N = 8;
+		double ans = Math.log(N) / Math.log(2);
+		int res = lg(N);
+		System.out.println(ans + " " + res);
+//		System.out.println(1<<4);
+//		System.out.println(1>>4);
+	}
+	
+	public static int lg(int N) {
+		int x = 0;
+		// N is integer , min(N) = 1;
+		if (N >= 1) {
+			while (1 << x < N)
+				x++;
+			if (1 << x > N) 
+				x -=1;
+		}
+		return x;
+	}
+}
